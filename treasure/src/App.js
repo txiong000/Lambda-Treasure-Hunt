5cb789fa765c23c1f1c9730dca5a5f673a947165
txiong000 <tkx422@gmail.com>
2019-02-12 12:23:09 -0600

    axio get request for token

--- treasure/src/App.js
+++ treasure/src/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+const API_KEY = process.env.REACT_API_KEY
 
 class App extends Component {
+  
+  componentDidMount = () => {
+    const headers = {headers:{Authorization: API_KEY }}
+    axios
+    .get('https://lambda-treasure-hunt.herokuapp.com/api/adv/init/', headers)
+    .then(response => {
+      console.log(response.data)
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
       </div>
     );
   }
